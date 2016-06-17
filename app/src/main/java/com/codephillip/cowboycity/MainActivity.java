41165819a3d215c5892237cee57262a30c2f5b52
codephillip <codephillip@gmail.com>
2016-06-17 15:49:55 +0300

    reduced cat size, used jumpModifier

--- app/src/main/java/com/codephillip/cowboycity/MainActivity.java
+++ app/src/main/java/com/codephillip/cowboycity/MainActivity.java
@@ -1,5 +1,6 @@
 package com.codephillip.cowboycity;
 
+import android.util.Log;
 import android.view.MotionEvent;
 
 import org.andengine.engine.camera.Camera;
@@ -9,7 +10,9 @@ import org.andengine.engine.options.ScreenOrientation;
 import org.andengine.engine.options.resolutionpolicy.FillResolutionPolicy;
 import org.andengine.entity.IEntity;
 import org.andengine.entity.modifier.IEntityModifier;
-import org.andengine.entity.modifier.MoveModifier;
+import org.andengine.entity.modifier.JumpModifier;
+import org.andengine.entity.modifier.LoopEntityModifier;
+import org.andengine.entity.modifier.MoveXModifier;
 import org.andengine.entity.scene.IOnSceneTouchListener;
 import org.andengine.entity.scene.Scene;
 import org.andengine.entity.sprite.AnimatedSprite;
@@ -38,6 +41,10 @@ public class MainActivity extends BaseGameActivity implements IOnSceneTouchListe
     private ITextureRegion backgroundTextureRegion;
     private Sprite backgroundSprite;
 
+    private BitmapTextureAtlas catTextureAtlas;
+    private ITiledTextureRegion catTiledTextureRegion;
+    private AnimatedSprite catAnimatedSprite;
+
     boolean canGo = false;
 
     public static final String TAG = "Cowboy#";
@@ -63,22 +70,32 @@ public class MainActivity extends BaseGameActivity implements IOnSceneTouchListe
         cowboyTiledTextureRegion = BitmapTextureAtlasTextureRegionFactory.createTiledFromAsset(cowboyTextureAtlas, this, "walkman640x320.png", 0, 0, 8, 1);
         cowboyTextureAtlas.load();
 
+        catTextureAtlas = new BitmapTextureAtlas(mEngine.getTextureManager(), 384, 384, TextureOptions.BILINEAR_PREMULTIPLYALPHA);
+        catTiledTextureRegion = BitmapTextureAtlasTextureRegionFactory.createTiledFromAsset(catTextureAtlas,this, "runningcat_reverse384.png", 0, 0, 2, 4);
+        catTextureAtlas.load();
+
         pOnCreateResourcesCallback.onCreateResourcesFinished();
     }
 
     @Override
     public void onCreateScene(OnCreateSceneCallback pOnCreateSceneCallback) throws IOException {
+        /**
+         * Transferred the code to onPopulateScene()
+         * */
+        scene = new Scene();
+        scene.setOnSceneTouchListener(this);
+        pOnCreateSceneCallback.onCreateSceneFinished(scene);
+    }
+
+    @Override
+    public void onPopulateScene(Scene pScene, OnPopulateSceneCallback pOnPopulateSceneCallback) throws IOException {
 
         final float positionX = CAMERA_WIDTH * 0.5f;
         final float positionY = CAMERA_HEIGHT * 0.5f;
 
-
-        scene = new Scene();
-        scene.setOnSceneTouchListener(this);
-
         backgroundSprite = new Sprite(positionX, positionY, backgroundTextureRegion, mEngine.getVertexBufferObjectManager());
 
-        cowboyAnimatedSprite = new AnimatedSprite(120, 120, cowboyTiledTextureRegion, mEngine.getVertexBufferObjectManager()){
+        cowboyAnimatedSprite = new AnimatedSprite(120, 100, cowboyTiledTextureRegion, mEngine.getVertexBufferObjectManager()){
 
             @Override
             protected void onManagedUpdate(float pSecondsElapsed) {
@@ -88,15 +105,38 @@ public class MainActivity extends BaseGameActivity implements IOnSceneTouchListe
         };
         cowboyAnimatedSprite.animate(50);
 
+        catAnimatedSprite = new AnimatedSprite(600, 90, catTiledTextureRegion, mEngine.getVertexBufferObjectManager()){
+            @Override
+            protected void onManagedUpdate(float pSecondsElapsed) {
+
+                /* Obtain the current x/y values */
+                final float currentX = this.getX();
+                final float currentY = this.getY();
+
+				/* If the two rectangle's are colliding, set this rectangle's color to GREEN */
+                if(this.collidesWith(cowboyAnimatedSprite)){
+                    Log.d(TAG, "onManagedUpdate: COLLISTION");
+                }
+
+                super.onManagedUpdate(pSecondsElapsed);
+
+            }
+        };
+
+        final MoveXModifier moveXModifier = new MoveXModifier(1.8f, 900, -150);
+        catAnimatedSprite.registerEntityModifier(new LoopEntityModifier(moveXModifier));
+
+        catAnimatedSprite.animate(100);
+
         scene.attachChild(backgroundSprite);
         scene.attachChild(cowboyAnimatedSprite);
+        scene.attachChild(catAnimatedSprite);
 //        scene.setBackground(new Background(Color.CYAN));
         scene.registerUpdateHandler(new IUpdateHandler() {
             @Override
             public void reset() {}
             @Override
             public void onUpdate(float pSecondsElapsed) {
-                // TODO Auto-generated method stub
                 //moves character 2 steps forward along x-axis
 //                if (canGo){
 //                    cowboyAnimatedSprite.setPosition(cowboyAnimatedSprite.getX() + 2, cowboyAnimatedSprite.getY());
@@ -107,7 +147,7 @@ public class MainActivity extends BaseGameActivity implements IOnSceneTouchListe
 ////                    cowboyAnimatedSprite.setPosition(cowboyAnimatedSprite.getX(), cowboyAnimatedSprite.getY()-8);
 //                }
 
-                final float innitialYPosition = 120;
+                final float innitialYPosition = 100;
 //                if (pSecondsElapsed < 0.2){
 //                    innitialYPosition = cowboyAnimatedSprite.getY();
 //                }
@@ -118,28 +158,42 @@ public class MainActivity extends BaseGameActivity implements IOnSceneTouchListe
                     final float fromX = cowboyAnimatedSprite.getX();
                     final float toX = cowboyAnimatedSprite.getX();
                     final float fromY = cowboyAnimatedSprite.getY();
-                    final float toY = cowboyAnimatedSprite.getY() + 80;
+                    final float toY = cowboyAnimatedSprite.getY();
+//                    final float toY = cowboyAnimatedSprite.getY() + 140;
 
 
                     //you can move in on axis using MoveYModifier or MoveXModifier
 //                    MoveYModifier mod1=new MoveYModifier(constanttime,fromY,toY);
 //                    sprite.registerEntityModifier(mod1);
 
-                    final MoveModifier downMoveModifier = new MoveModifier(0.4f, fromX, fromY, toX, innitialYPosition, new IEntityModifier.IEntityModifierListener() {
-                        @Override
-                        public void onModifierStarted(IModifier<IEntity> pModifier, IEntity pItem) {
-                            cowboyAnimatedSprite.setCurrentTileIndex(4);
-                        }
+//                    final MoveModifier downMoveModifier = new MoveModifier(0.8f, fromX, fromY, toX, innitialYPosition, new IEntityModifier.IEntityModifierListener() {
+//                        @Override
+//                        public void onModifierStarted(IModifier<IEntity> pModifier, IEntity pItem) {
+////                            cowboyAnimatedSprite.setCurrentTileIndex(4);
+//                        }
+//
+//                        @Override
+//                        public void onModifierFinished(IModifier<IEntity> pModifier, IEntity pItem) {
+////                            cowboyAnimatedSprite.stopAnimation(2);
+////                            cowboyAnimatedSprite.setCurrentTileIndex(2);
+//                            cowboyAnimatedSprite.animate(50);
+//                        }
+//                    });
+//
+//                    final MoveModifier upMoveModifier = new MoveModifier(0.4f, fromX, fromY, toX, toY, new IEntityModifier.IEntityModifierListener() {
+//                        @Override
+//                        public void onModifierStarted(IModifier<IEntity> pModifier, IEntity pItem) {
+//                            cowboyAnimatedSprite.stopAnimation(0);
+//                        }
+//
+//                        @Override
+//                        public void onModifierFinished(IModifier<IEntity> pModifier, IEntity pItem) {
+//                            cowboyAnimatedSprite.registerEntityModifier(downMoveModifier);
+//                        }
+//                    });
 
-                        @Override
-                        public void onModifierFinished(IModifier<IEntity> pModifier, IEntity pItem) {
-//                            cowboyAnimatedSprite.stopAnimation(2);
-//                            cowboyAnimatedSprite.setCurrentTileIndex(2);
-                            cowboyAnimatedSprite.animate(50);
-                        }
-                    });
-
-                    final MoveModifier upMoveModifier = new MoveModifier(0.3f, fromX, fromY, toX, toY, new IEntityModifier.IEntityModifierListener() {
+                    //-140 means jump upward, positive move downward
+                    JumpModifier jumpModifier = new JumpModifier(0.8f, fromX, toX, fromY, toY, -140, new IEntityModifier.IEntityModifierListener() {
                         @Override
                         public void onModifierStarted(IModifier<IEntity> pModifier, IEntity pItem) {
                             cowboyAnimatedSprite.stopAnimation(0);
@@ -147,19 +201,18 @@ public class MainActivity extends BaseGameActivity implements IOnSceneTouchListe
 
                         @Override
                         public void onModifierFinished(IModifier<IEntity> pModifier, IEntity pItem) {
-                            cowboyAnimatedSprite.registerEntityModifier(downMoveModifier);
+                            cowboyAnimatedSprite.animate(50);
+                            cowboyAnimatedSprite.setY(innitialYPosition);
                         }
                     });
 
-                    cowboyAnimatedSprite.registerEntityModifier(upMoveModifier);
+                    cowboyAnimatedSprite.registerEntityModifier(jumpModifier);
+//                    cowboyAnimatedSprite.registerEntityModifier(upMoveModifier);
                 }
             }
         });
-        pOnCreateSceneCallback.onCreateSceneFinished(scene);
-    }
 
-    @Override
-    public void onPopulateScene(Scene pScene, OnPopulateSceneCallback pOnPopulateSceneCallback) throws IOException {
+
         pOnPopulateSceneCallback.onPopulateSceneFinished();
     }
 
