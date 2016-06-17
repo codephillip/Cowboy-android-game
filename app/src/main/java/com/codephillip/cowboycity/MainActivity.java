519d6f0cbff8b787b6d89db800c2b7792721447e
codephillip <codephillip@gmail.com>
2016-06-17 09:31:31 +0300

    added AndEngine, tested with red background

--- app/src/main/java/com/codephillip/cowboycity/MainActivity.java
+++ app/src/main/java/com/codephillip/cowboycity/MainActivity.java
@@ -1,13 +1,43 @@
 package com.codephillip.cowboycity;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import org.andengine.engine.camera.Camera;
+import org.andengine.engine.options.EngineOptions;
+import org.andengine.engine.options.ScreenOrientation;
+import org.andengine.engine.options.resolutionpolicy.FillResolutionPolicy;
+import org.andengine.entity.scene.Scene;
+import org.andengine.entity.scene.background.Background;
+import org.andengine.ui.activity.BaseGameActivity;
+import org.andengine.util.adt.color.Color;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.IOException;
+
+public class MainActivity extends BaseGameActivity {
+
+    private static final int CAMERA_WIDTH = 800;
+    private static final int CAMERA_HEIGHT = 480;
+
+    private Scene scene;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public EngineOptions onCreateEngineOptions() {
+        Camera camera = new Camera(0,0, CAMERA_WIDTH, CAMERA_HEIGHT);
+        return new EngineOptions(true, ScreenOrientation.LANDSCAPE_FIXED, new FillResolutionPolicy(), camera);
+    }
+
+    @Override
+    public void onCreateResources(OnCreateResourcesCallback pOnCreateResourcesCallback) throws IOException {
+        pOnCreateResourcesCallback.onCreateResourcesFinished();
+    }
+
+    @Override
+    public void onCreateScene(OnCreateSceneCallback pOnCreateSceneCallback) throws IOException {
+        scene = new Scene();
+        scene.setBackground(new Background(Color.RED));
+        pOnCreateSceneCallback.onCreateSceneFinished(scene);
+    }
+
+    @Override
+    public void onPopulateScene(Scene pScene, OnPopulateSceneCallback pOnPopulateSceneCallback) throws IOException {
+        pOnPopulateSceneCallback.onPopulateSceneFinished();
     }
 }
