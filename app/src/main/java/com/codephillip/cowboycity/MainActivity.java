072c3364afb1919ad4fe237a59603b6f69bacbe0
codephillip <codephillip@gmail.com>
2016-06-18 18:32:33 +0300

    added parallax backgrounds

--- app/src/main/java/com/codephillip/cowboycity/MainActivity.java
+++ app/src/main/java/com/codephillip/cowboycity/MainActivity.java
@@ -19,6 +19,7 @@ import org.andengine.entity.modifier.LoopEntityModifier;
 import org.andengine.entity.modifier.MoveXModifier;
 import org.andengine.entity.scene.IOnSceneTouchListener;
 import org.andengine.entity.scene.Scene;
+import org.andengine.entity.scene.background.ParallaxBackground;
 import org.andengine.entity.sprite.AnimatedSprite;
 import org.andengine.entity.sprite.Sprite;
 import org.andengine.input.touch.TouchEvent;
@@ -49,9 +50,23 @@ public class MainActivity extends BaseGameActivity implements IOnSceneTouchListe
     private ITiledTextureRegion catTiledTextureRegion;
     private AnimatedSprite catAnimatedSprite;
 
+    private BitmapTextureAtlas frontBitmapTextureAtlas;
+    private BitmapTextureAtlas midBitmapTextureAtlas;
+//    private BuildableBitmapTextureAtlas backBitmapTextureAtlas;
+    private BitmapTextureAtlas backBitmapTextureAtlas;
+    private ITextureRegion frontTextureRegion;
+    private ITextureRegion midTextureRegion;
+    private ITextureRegion backTextureRegion;
+
+    Sprite frontBackgroundSprite;
+
+    private Camera camera;
+
     boolean canGo = false;
     private boolean hasPlayed = false;
     private boolean hasPlayedJumpSound = false;
+
+    private static final float SCROLL_FACTOR = 10;
 
     public static final String TAG = "Cowboy#";
 
@@ -65,7 +80,80 @@ public class MainActivity extends BaseGameActivity implements IOnSceneTouchListe
 
     @Override
     public EngineOptions onCreateEngineOptions() {
-        Camera camera = new Camera(0,0, CAMERA_WIDTH, CAMERA_HEIGHT);
+        camera = new Camera(0,0, CAMERA_WIDTH, CAMERA_HEIGHT){
+
+        //PARALLAX CODE
+          /* Boolean value which will determine whether
+             * to increase or decrease x coordinate */
+        boolean incrementX = true;
+
+            /* On camera update... */
+        @Override
+        public void onUpdate(float pSecondsElapsed) {
+
+				/* Obtain the current camera X coordinate */
+            final float currentCenterX = this.getCenterX();
+
+				/* Value which will be used to offset the camera */
+            float offsetCenterX = 0;
+
+				/* If incrementX is true... */
+//                if(incrementX){
+//
+//					/* offset the camera's x coordinate according to time passed */
+//                    offsetCenterX = currentCenterX + pSecondsElapsed * SCROLL_FACTOR;
+//
+//					/* If the new offset coordinate is greater than the max X limit */
+//                    if(offsetCenterX >= CAMERA_MAX_CENTER_X){
+//
+//						/* Set to decrement the camera's X coordinate next */
+//                        incrementX = false;
+//                    }
+//                } else {
+//					/* If increment is equal to false, decrement X coordinate */
+//                    offsetCenterX = currentCenterX - pSecondsElapsed * SCROLL_FACTOR;
+//
+//					/* If the new offset coordinate is less than the min X limit */
+//                    if(offsetCenterX <= CAMERA_MIN_CENTER_X){
+//
+//						/* Set to increment the camera's X coordinate next */
+//                        incrementX = true;
+//                    }
+//                }
+
+
+                /* If incrementX is true... */
+            if(false){
+
+					/* offset the camera's x coordinate according to time passed */
+                offsetCenterX = currentCenterX + pSecondsElapsed * SCROLL_FACTOR;
+
+					/* If the new offset coordinate is greater than the max X limit */
+//                    if(offsetCenterX >= CAMERA_MAX_CENTER_X){
+//
+//						/* Set to decrement the camera's X coordinate next */
+//                        incrementX = false;
+//                    }
+            } else {
+//					/* If increment is equal to false, decrement X coordinate */
+                offsetCenterX = currentCenterX - pSecondsElapsed * SCROLL_FACTOR;
+//
+//					/* If the new offset coordinate is less than the min X limit */
+//                    if(offsetCenterX <= CAMERA_MIN_CENTER_X){
+//
+//						/* Set to increment the camera's X coordinate next */
+//                        incrementX = true;
+//                    }
+            }
+
+				/* Apply the offset position to the camera */
+            this.setCenter(offsetCenterX, this.getCenterY());
+
+            super.onUpdate(pSecondsElapsed);
+        }
+    };
+
+
         EngineOptions engineOptions = new EngineOptions(true, ScreenOrientation.LANDSCAPE_FIXED, new FillResolutionPolicy(), camera);
         engineOptions.getAudioOptions().setNeedsSound(true);
         engineOptions.getAudioOptions().setNeedsMusic(true);
@@ -87,6 +175,36 @@ public class MainActivity extends BaseGameActivity implements IOnSceneTouchListe
         catTextureAtlas = new BitmapTextureAtlas(mEngine.getTextureManager(), 384, 384, TextureOptions.BILINEAR_PREMULTIPLYALPHA);
         catTiledTextureRegion = BitmapTextureAtlasTextureRegionFactory.createTiledFromAsset(catTextureAtlas,this, "runningcat_reverse384.png", 0, 0, 2, 4);
         catTextureAtlas.load();
+
+        frontBitmapTextureAtlas = new BitmapTextureAtlas(mEngine.getTextureManager(), 640, 187, TextureOptions.DEFAULT);
+        frontTextureRegion = BitmapTextureAtlasTextureRegionFactory.createFromAsset(frontBitmapTextureAtlas, this, "parallax_background_layer_front.png", 0, 0);
+        frontBitmapTextureAtlas.load();
+
+        midBitmapTextureAtlas = new BitmapTextureAtlas(mEngine.getTextureManager(), 640, 85, TextureOptions.DEFAULT);
+        midTextureRegion = BitmapTextureAtlasTextureRegionFactory.createFromAsset(midBitmapTextureAtlas, this, "parallax_background_layer_mid.png", 0, 0);
+        midBitmapTextureAtlas.load();
+
+        backBitmapTextureAtlas = new BitmapTextureAtlas(mEngine.getTextureManager(), 256, 128, TextureOptions.DEFAULT);
+//        backBitmapTextureAtlas = new BuildableBitmapTextureAtlas(mEngine.getTextureManager(), 256, 128, TextureOptions.DEFAULT);
+//        try {
+//            backBitmapTextureAtlas.build(new BlackPawnTextureAtlasBuilder<IBitmapTextureAtlasSource, BitmapTextureAtlas>(
+//                    0, 0, 0));
+//        } catch (ITextureAtlasBuilder.TextureAtlasBuilderException e) {
+//            e.printStackTrace();
+//        }
+        backTextureRegion = BitmapTextureAtlasTextureRegionFactory.createFromAsset(backBitmapTextureAtlas, this, "cloud.png", 0, 0);
+
+        /* Build the texture atlas. Since we're loading only a
+		 * single image into the texture atlas, we need not
+		 * worry about padding or spacing values */
+//        try {
+//            backBitmapTextureAtlas.build(new BlackPawnTextureAtlasBuilder<IBitmapTextureAtlasSource, BitmapTextureAtlas>(
+//                    0, 0, 0));
+//        } catch (ITextureAtlasBuilder.TextureAtlasBuilderException e) {
+//            e.printStackTrace();
+//        }
+        backBitmapTextureAtlas.load();
+
 
         try {
             biteSound = SoundFactory.createSoundFromAsset(getEngine().getSoundManager(), this, "mfx/lion.ogg");
@@ -115,6 +233,84 @@ public class MainActivity extends BaseGameActivity implements IOnSceneTouchListe
          * Transferred the code to onPopulateScene()
          * */
         scene = new Scene();
+
+        final float textureHeight = frontTextureRegion.getHeight();
+
+        /* Create the hill which will appear to be the furthest
+		 * into the distance. This Sprite will be placed higher than the
+		 * rest in order to retain visibility of it */
+        Sprite backBackgroundSprite = new Sprite(CAMERA_WIDTH * 0.5f, textureHeight * 0.5f + 250, backTextureRegion,
+                mEngine.getVertexBufferObjectManager());
+
+		/* Create the hill which will appear between the furthest and closest
+		 * hills. This Sprite will be placed higher than the closest hill, but
+		 * lower than the furthest hill in order to retain visibility */
+        Sprite midBackgroundSprite = new Sprite(CAMERA_WIDTH * 0.5f, textureHeight * 0.5f + 75, midTextureRegion,
+                mEngine.getVertexBufferObjectManager());
+
+		/* Create the closest hill which will not be obstructed by any other hill
+		 * Sprites. This Sprite will be placed at the bottom of the Scene since
+		 * nothing will be covering its view */
+        frontBackgroundSprite = new Sprite(CAMERA_WIDTH * 0.5f, textureHeight * 0.5f + 35, frontTextureRegion,
+                mEngine.getVertexBufferObjectManager());
+
+        final float positionX = CAMERA_WIDTH * 0.5f;
+        final float positionY = CAMERA_HEIGHT * 0.5f;
+
+        backgroundSprite = new Sprite(positionX, positionY, backgroundTextureRegion, mEngine.getVertexBufferObjectManager());
+
+
+        /* Create the ParallaxBackground, setting the color values to represent
+		 * a blue sky */
+        ParallaxBackground background = new ParallaxBackground(0.3f, 1f, 0.9f) {
+
+            /* We'll use these values to calculate the
+             * parallax value of the background */
+            float cameraPreviousX = 0;
+            float parallaxValueOffset = 0;
+
+            /* onUpdates to the background, we need to calculate new
+             * parallax values in order to apply movement to the background
+             * objects (the hills in this case) */
+            @Override
+            public void onUpdate(float pSecondsElapsed) {
+
+				/* Obtain the camera's current center X value */
+                final float cameraCurrentX = camera.getCenterX();
+
+				/* If the camera's position has changed since last
+				 * update... */
+                if (cameraPreviousX != cameraCurrentX) {
+
+					/* Calculate the new parallax value offset by
+					 * subtracting the previous update's camera x coordinate
+					 * from the current update's camera x coordinate */
+                    parallaxValueOffset +=  cameraCurrentX - cameraPreviousX;
+
+					/* Apply the parallax value offset to the background, which
+					 * will in-turn offset the positions of entities attached
+					 * to the background */
+                    this.setParallaxValue(parallaxValueOffset);
+
+					/* Update the previous camera X since we're finished with this
+					 * update */
+                    cameraPreviousX = cameraCurrentX;
+                }
+
+                super.onUpdate(pSecondsElapsed);
+            }
+        };
+
+
+        background.attachParallaxEntity(new ParallaxBackground.ParallaxEntity(0, backgroundSprite));
+        background.attachParallaxEntity(new ParallaxBackground.ParallaxEntity(5, backBackgroundSprite));
+        background.attachParallaxEntity(new ParallaxBackground.ParallaxEntity(10, midBackgroundSprite));
+        background.attachParallaxEntity(new ParallaxBackground.ParallaxEntity(15, frontBackgroundSprite));
+
+		/* Set & Enabled the background */
+        scene.setBackground(background);
+        scene.setBackgroundEnabled(true);
+
         scene.setOnSceneTouchListener(this);
         gameSound.play();
         footstepSound.play();
@@ -124,10 +320,10 @@ public class MainActivity extends BaseGameActivity implements IOnSceneTouchListe
     @Override
     public void onPopulateScene(Scene pScene, OnPopulateSceneCallback pOnPopulateSceneCallback) throws IOException {
 
-        final float positionX = CAMERA_WIDTH * 0.5f;
-        final float positionY = CAMERA_HEIGHT * 0.5f;
-
-        backgroundSprite = new Sprite(positionX, positionY, backgroundTextureRegion, mEngine.getVertexBufferObjectManager());
+//        final float positionX = CAMERA_WIDTH * 0.5f;
+//        final float positionY = CAMERA_HEIGHT * 0.5f;
+//
+//        backgroundSprite = new Sprite(positionX, positionY, backgroundTextureRegion, mEngine.getVertexBufferObjectManager());
 
         cowboyAnimatedSprite = new AnimatedSprite(120, 100, cowboyTiledTextureRegion, mEngine.getVertexBufferObjectManager()){
 
@@ -149,7 +345,7 @@ public class MainActivity extends BaseGameActivity implements IOnSceneTouchListe
                 final float currentY = this.getY();
 
 				/* If the two rectangle's are colliding, set this rectangle's color to GREEN */
-                Log.d(TAG, "onManagedUpdate: ###"+this.getX()+"and Y "+ this.getY());
+//                Log.d(TAG, "onManagedUpdate: ###"+this.getX()+"and Y "+ this.getY());
                 if(this.collidesWith(cowboyAnimatedSprite) && this.getX() >= 180){
                     Log.d(TAG, "onManagedUpdate: COLLISTION");
                     if (!hasPlayed){
@@ -172,9 +368,9 @@ public class MainActivity extends BaseGameActivity implements IOnSceneTouchListe
 
         catAnimatedSprite.animate(100);
 
-        scene.attachChild(backgroundSprite);
+//        scene.attachChild(backgroundSprite);
         scene.attachChild(cowboyAnimatedSprite);
-        scene.attachChild(catAnimatedSprite);
+        frontBackgroundSprite.attachChild(catAnimatedSprite);
 //        scene.setBackground(new Background(Color.CYAN));
 
         final float fromX = cowboyAnimatedSprite.getX();
